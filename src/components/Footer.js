54021c3e969e302f942a5f2881d2b12a24973f0b
Harshith Venkatesh <harshithece7016@gmail.com>
2022-02-11 16:31:35 +0530

    automobile page routes changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,8 +29,8 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/about">
-                        About
+                      <Link className="navbar-item" to="/automobile">
+                        Automobile
                       </Link>
                     </li>
                     <li>
